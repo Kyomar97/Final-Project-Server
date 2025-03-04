170a675a09879c412b6fbb04b55faa7a0689d530
Sebna Omar <omargr97@gmail.com>
2025-03-04 19:34:18 +0000

    user model connect

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 require("dotenv").config();
 
 // ℹ️ Connects to the database
-require("./db");
+require("./db/index");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+require("./models/User.model");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
