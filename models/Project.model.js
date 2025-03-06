93905112b0c7b99864d950461025ba1b909f0f18
Sebna Omar <omargr97@gmail.com>
2025-03-06 10:24:32 +0000

    Project.model created

---
+++ models/Project.model.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require("mongoose");
+
+const projectSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: [true, "El nombre del programa es obligatorio."],
+      trim: true,
+    },
+    descripcion: {
+      type: String,
+      required: [true, "La descripción del programa es obligatoria."],
+    },
+    objetivo: {
+      type: String,
+      required: [true, "El objetivo del programa es obligatorio."],
+    },
+    ubicacion: {
+      isla: {
+        type: String,
+        required: [true, "La isla es obligatoria."],
+      },
+      municipio: {
+        type: String,
+        required: [true, "El municipio es obligatorio."],
+      },
+    },
+    fecha_inicio: {
+      type: Date,
+      required: [true, "La fecha de inicio es obligatoria."],
+    },
+    fecha_fin: {
+      type: Date,
+      required: [true, "La fecha de finalización es obligatoria."],
+    },
+    estado: {
+      type: String,
+      required: [true, "El estado del programa es obligatorio."],
+      enum: ["activo", "pendiente", "finalizado", "cancelado"],
+      default: "pendiente",
+    },
+    beneficiarios: {
+      type: Number,
+      default: 0,
+    },
+    recursos: {
+      presupuesto: {
+        type: Number,
+      },
+      financiadores: {
+        type: [String],
+        default: [],
+      },
+      voluntarios: {
+        type: Number,
+        default: 0,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Projects = model("Project", projectSchema);
+
+module.exports = Projects;
